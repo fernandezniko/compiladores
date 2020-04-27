7fcabc6cc3e43ba0c82175641f28a05eb9766358
retr8 <fernando.rad@gmail.com>
2020-04-26 21:21:20 -0300

    Actualizada la prueba.txt

--- Primera Entrega/generar.bat
+++ Primera Entrega/generar.bat
@@ -4,7 +4,8 @@ c:\GnuWin32\bin\bison -dyv sintactico.y
 pause
 c:\MinGW\bin\gcc.exe ts.c lex.yy.c y.tab.c -o primera.exe
 pause
-primera.exe prueba_ENTSAL.txt
+primera.exe prueba2.txt
+pause
 del lex.yy.c
 del y.tab.c
 del y.output
