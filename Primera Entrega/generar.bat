d16fc1eb6a2dd4317c216d81163be34012c85144
Nico Fernandez <ni.ko94@hotmail.com>
2020-05-04 16:53:00 -0300

    se corrige el generar.bat y se agrega ejecutable

--- Primera Entrega/generar.bat
+++ Primera Entrega/generar.bat
@@ -1,13 +1,15 @@
-@echo off
-del y.output
-c:\GnuWin32\bin\flex -d lexico.l
+c:\GnuWin32\bin\flex lexico.l
 
 c:\GnuWin32\bin\bison -dyv sintactico.y 
 
 c:\MinGW\bin\gcc.exe ts.c lex.yy.c y.tab.c -o primera.exe
+pause
 primera.exe prueba.txt
 pause
-@echo off
+
 
 del lex.yy.c
 del y.tab.c
+del y.output
+del y.tab.h
+pause
